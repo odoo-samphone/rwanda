cd8d1f4129ccd1a0b41734dc8daa980f9582c96d
sufyanel <sufyanelahi@gmail.com>
2022-08-01 17:10:25 +0500

    [ADD] updated code of pos_user_restrict module

--- pos_user_restrict/models/res_users.py
+++ pos_user_restrict/models/res_users.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+# Copyright © 2018 Garazd Creation (<https://garazd.biz>)
+# @author: Yurii Razumovskyi (<support@garazd.biz>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
 
 from odoo import fields, models
 
@@ -9,13 +11,12 @@ class ResUsers(models.Model):
     pos_config_ids = fields.Many2many(
         comodel_name='pos.config',
         string='Allowed POS',
-        help="Allowed Points of Sales for the user. POS managers can use all POS.",
+        help="Allowed Points of Sales for the user. "
+             "POS managers can use all POS.",
     )
 
     def write(self, values):
         res = super(ResUsers, self).write(values)
         if self.ids and 'pos_config_ids' in values:
-            self.env['ir.model.access'].call_cache_clearing_methods()
             self.env['ir.rule'].clear_caches()
-            # self.has_group.clear_cache(self) -- commented out as it was raising error, that clear_cache doesn't exist
         return res
